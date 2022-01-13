307e2fa89e3164d5e485ccc7235314f24cf02dc9
D_Dos <46603505+Ddoskhozhina@users.noreply.github.com>
2022-01-13 18:23:39 +0600

    Add files via upload
    Election smart contract

--- election.sol
+++ election.sol
@@ -1,30 +1,62 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 
-contract Election{
-    uint[] proposals;
-    mapping(address => bool) voters;
-    
-    constructor(uint proposalsCount) {
-        proposals.length = proposalsCount;
+contract Election {
+
+    struct Candidate {
+        string name;
+        uint voteCount;
     }
 
-    function vote(uint proposal) public {
-        bool hasVoted = voters[msg.sender];
-        require (!hasVoted && proposal < proposals.length);
-        voters[msg.sender] = true;
-        proposals[proposal]++;
+    struct Voter {
+        bool authorized;
+        bool voted;
+        uint vote;
     }
 
-    function winningProposal() public view returns (uint _winningProposal){
-        uint winningVoteCount = 0;
-        for (uint prop = 0; prop < proposals.length; prop++){
-            uint voteCount = proposals[prop];
-            if (voteCount > winningVoteCount){
-                winningVoteCount = voteCount;
-                _winningProposal = prop;
-            }
-        }
+    address public owner;
+    string public electionName;
+
+    mapping(address => Voter) public voters;
+    Candidate[] public candidates;
+    uint public totalVotes;
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
     }
+
+    function Election(string _name) public {
+        owner = msg.sender;
+        electionName = _name;
+    }
+
+    function addCandidate(string _name) ownerOnly public {
+        candidates.push(Candidate(_name, 0));
+    }
+
+    function getNumCandidate() public view returns(uint) {
+        returns candidates.length;
+    }
+
+    function authorize(address _person) ownerOnly public {
+        voters[_person].authorized = true;
+    }
+
+    function vote(uint _voteIndex) public {
+        require(!voters[msg.sender].voted);
+        require(voters[msg.sender].authorized);
+
+        voters[msg.sender].vote = _voteIndex;
+        voters[msg.sender].voted = true;
+
+        candidates[_voteIndex].voteCount += 1;
+        totalVotes += 1;
+    }
+
+    function end() ownerOnly public {
+        selfdestruct(owner);
+    }
+
 }
